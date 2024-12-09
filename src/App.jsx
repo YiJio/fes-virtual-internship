28e6fa5a29edd8e20e8a9b07700f73b70be902a7
unknown <jy.68171@nevada.unr.edu>
2024-12-09 13:18:45 -0800

    implemented hot collections feature for home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,27 @@
-import Home from "./pages/Home";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Explore from "./pages/Explore";
-import Author from "./pages/Author";
-import ItemDetails from "./pages/ItemDetails";
-import Nav from "./components/Nav";
-import Footer from "./components/Footer";
+// packages
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// css
+import 'react-loading-skeleton/dist/skeleton.css'
+// pages
+import Home from './pages/Home';
+import Explore from './pages/Explore';
+import Author from './pages/Author';
+import ItemDetails from './pages/ItemDetails';
+// components
+import Nav from './components/Nav';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <Router>
       <Nav />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/explore" element={<Explore />} />
-        <Route path="/author" element={<Author />} />
-        <Route path="/item-details" element={<ItemDetails />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/explore' element={<Explore />} />
+        <Route path='/author' element={<Author />} />
+        <Route path='/author/:id' element={<Author />} />
+        <Route path='/item-details' element={<ItemDetails />} />
+        <Route path='/item-details/:id' element={<ItemDetails />} />
       </Routes>
       <Footer />
     </Router>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,23 @@
-import React, { useEffect } from "react";
-import BrowseByCategory from "../components/home/BrowseByCategory";
-import HotCollections from "../components/home/HotCollections";
-import Landing from "../components/home/Landing";
-import LandingIntro from "../components/home/LandingIntro";
-import NewItems from "../components/home/NewItems";
-import TopSellers from "../components/home/TopSellers";
+// packages
+import React, { useEffect } from 'react';
+// components
+import BrowseByCategory from '../components/home/BrowseByCategory';
+import HotCollections from '../components/home/HotCollections';
+import Landing from '../components/home/Landing';
+import LandingIntro from '../components/home/LandingIntro';
+import NewItems from '../components/home/NewItems';
+import TopSellers from '../components/home/TopSellers';
 
 const Home = () => {
+  
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
 
   return (
-    <div id="wrapper">
-      <div className="no-bottom no-top" id="content">
-        <div id="top"></div>
+    <div id='wrapper'>
+      <div className='no-bottom no-top' id='content'>
+        <div id='top'></div>
         <Landing />
         <LandingIntro />
         <HotCollections />
