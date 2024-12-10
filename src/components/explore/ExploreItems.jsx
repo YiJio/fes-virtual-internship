2eea7c74b6dea81086fb0e5010c0b0d3341c51d1
unknown <jy.68171@nevada.unr.edu>
2024-12-09 18:25:21 -0800

    use filtering from api and reusable

--- src/components/explore/ExploreItems.jsx
+++ src/components/explore/ExploreItems.jsx
@@ -1,32 +1,15 @@
 // packages
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import Skeleton from 'react-loading-skeleton';
 import axios from 'axios';
 // components
-import Countdown from '../UI/Countdown';
+import { NFTItemCard, NFTItemCardSkeleton } from '../UI/NFTItemCard';
 
 const ExploreItemsSkeleton = () => {
   return (
     <>
       {new Array(8).fill(0).map((_, index) => (
         <div key={index} className='d-item col-lg-3 col-md-6 col-sm-6 col-xs-12 skeleton' style={{ display: 'block', backgroundSize: 'cover' }}>
-          <div key={index} className='nft__item'>
-            <div className='author_list_pp'>
-              <Skeleton circle />
-              <i className='fa fa-check' />
-            </div>
-            <div className='nft__item_wrap'>
-              <Skeleton count={1} />
-            </div>
-            <div className='nft__item_info'>
-              <Skeleton count={1} width='40%' height='18px' />
-              <Skeleton count={1} width='25%' height='14px' />
-              <div className='nft__item_like'>
-                <i className='fa fa-heart' />
-              </div>
-            </div>
-          </div>
+          <NFTItemCardSkeleton />
         </div>
       ))}
     </>
@@ -58,26 +41,28 @@ const ExploreItems = () => {
     }
   }
 
-  useEffect(() => {
-    const fetchItems = async () => {
-      // fetch API
-      try {
-        const resp = await axios.get('https://us-central1-nft-cloud-functions.cloudfunctions.net/explore');
-        setItems(resp.data);
-        setUiFilteredItems(resp.data);
-        setTimeout(() => {
-          setUiIsLoading(false);
-        }, 500);
-      } catch (error) {
-        console.error('Error fetching new items: ', error);
+  const fetchItems = async () => {
+    // fetch API
+    try {
+      const resp = await axios.get(`https://us-central1-nft-cloud-functions.cloudfunctions.net/explore${ui_filter !== '' ? `?filter=${ui_filter}` : ''}`);
+      setItems(resp.data);
+      //setUiFilteredItems(resp.data);
+      setTimeout(() => {
         setUiIsLoading(false);
-      }
+      }, 500);
+    } catch (error) {
+      console.error('Error fetching new items: ', error);
+      setUiIsLoading(false);
     }
+  }
+
+  useEffect(() => {    
     fetchItems();
   }, []);
 
   useEffect(() => {
-    filterItems();
+    //filterItems();
+    fetchItems();
   }, [ui_filter]);
 
   return (
@@ -90,49 +75,9 @@ const ExploreItems = () => {
           <option value='likes_high_to_low'>Most liked</option>
         </select>
       </div>
-      {!ui_isLoading && ui_filteredItems ? (<>
-        {ui_filteredItems?.slice(0, ui_numVisibleItems).map((item, index) => (<div key={index} className='d-item col-lg-3 col-md-6 col-sm-6 col-xs-12' style={{ display: 'block', backgroundSize: 'cover' }}>
-          <div className='nft__item'>
-            <div className='author_list_pp'>
-              <Link to={`/author/${item.authorId}`} data-bs-toggle='tooltip' data-bs-placement='top'>
-                <img className='lazy' src={item.authorImage} alt={`User ${item.authorId}`} />
-                <i className='fa fa-check' />
-              </Link>
-            </div>
-            <Countdown time={item.expiryDate} />
-            <div className='nft__item_wrap'>
-              <div className='nft__item_extra'>
-                <div className='nft__item_buttons'>
-                  <button>Buy Now</button>
-                  <div className='nft__item_share'>
-                    <h4>Share</h4>
-                    <a href='' target='_blank' rel='noreferrer'>
-                      <i className='fa fa-facebook fa-lg' />
-                    </a>
-                    <a href='' target='_blank' rel='noreferrer'>
-                      <i className='fa fa-twitter fa-lg' />
-                    </a>
-                    <a href=''>
-                      <i className='fa fa-envelope fa-lg' />
-                    </a>
-                  </div>
-                </div>
-              </div>
-              <Link to={`/item-details/${item.nftId}`}>
-                <img src={item.nftImage} className='lazy nft__item_preview' alt={`${item.title} image`} />
-              </Link>
-            </div>
-            <div className='nft__item_info'>
-              <Link to={`/item-details/${item.nftId}`}>
-                <h4>{item.title}</h4>
-              </Link>
-              <div className='nft__item_price'>{item.price} ETH</div>
-              <div className='nft__item_like'>
-                <i className='fa fa-heart'></i>
-                <span>{item.likes}</span>
-              </div>
-            </div>
-          </div>
+      {!ui_isLoading ? (<>
+        {_items?.slice(0, ui_numVisibleItems).map((item, index) => (<div key={index} className='d-item col-lg-3 col-md-6 col-sm-6 col-xs-12' style={{ display: 'block', backgroundSize: 'cover' }}>
+          <NFTItemCard data={item} />
         </div>))}
       </>) : <ExploreItemsSkeleton />}
       {(!ui_isLoading && ui_numVisibleItems < _items.length) && <div className='col-md-12 text-center'>
--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -1,34 +1,15 @@
 // packages
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import Skeleton from 'react-loading-skeleton';
 import OwlCarousel from 'react-owl-carousel';
 import axios from 'axios';
 // components
-import Countdown from '../UI/Countdown';
+import { NFTItemCard, NFTItemCardSkeleton } from '../UI/NFTItemCard';
 
 const NewItemsSkeleton = () => {
   return (
     <div className='owl-carousel owl-theme skeleton'>
       <div className='owl-stage-outer'>
-        {new Array(4).fill(0).map((_, index) => (
-          <div key={index} className='nft__item'>
-            <div className='author_list_pp'>
-              <Skeleton circle />
-              <i className='fa fa-check' />
-            </div>
-            <div className='nft__item_wrap'>
-              <Skeleton count={1} />
-            </div>
-            <div className='nft__item_info'>
-              <Skeleton count={1} width='40%' height='18px' />
-              <Skeleton count={1} width='25%' height='14px' />
-              <div className='nft__item_like'>
-                <i className='fa fa-heart' />
-              </div>
-            </div>
-          </div>
-        ))}
+        {new Array(4).fill(0).map((_, index) => (<NFTItemCardSkeleton key={index} />))}
       </div>
       <div className='owl-nav'>
         <button type='button' role='presentation' className='owl-prev'><span aria-label='Previous'>‹</span></button>
@@ -79,49 +60,7 @@ const NewItems = () => {
             </div>
           </div>
           {ui_isLoading ? <NewItemsSkeleton /> : <OwlCarousel className='owl-theme' loop margin={10} nav responsive={carouselOptionsResponsive}>
-            {_items?.map((item, index) => (<React.Fragment key={index}>
-              <div className='nft__item'>
-                <div className='author_list_pp'>
-                  <Link to={`/author/${item.authorId}`} data-bs-toggle='tooltip' data-bs-placement='top' title='Creator: Monica Lucas'>
-                    <img className='lazy' src={item.authorImage} alt={`User ${item.authorId}`} />
-                    <i className='fa fa-check'></i>
-                  </Link>
-                </div>
-                <Countdown time={item.expiryDate} />
-                <div className='nft__item_wrap'>
-                  <div className='nft__item_extra'>
-                    <div className='nft__item_buttons'>
-                      <button>Buy Now</button>
-                      <div className='nft__item_share'>
-                        <h4>Share</h4>
-                        <a href='' target='_blank' rel='noreferrer'>
-                          <i className='fa fa-facebook fa-lg'></i>
-                        </a>
-                        <a href='' target='_blank' rel='noreferrer'>
-                          <i className='fa fa-twitter fa-lg'></i>
-                        </a>
-                        <a href=''>
-                          <i className='fa fa-envelope fa-lg'></i>
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-                  <Link to={`/item-details/${item.nftId}`}>
-                    <img src={item.nftImage} className='lazy nft__item_preview' alt={`${item.title} image`} />
-                  </Link>
-                </div>
-                <div className='nft__item_info'>
-                  <Link to={`/item-details/${item.nftId}`}>
-                    <h4>{item.title}</h4>
-                  </Link>
-                  <div className='nft__item_price'>{item.price} ETH</div>
-                  <div className='nft__item_like'>
-                    <i className='fa fa-heart'></i>
-                    <span>{item.likes}</span>
-                  </div>
-                </div>
-              </div>
-            </React.Fragment>))}
+            {_items?.map((item, index) => (<NFTItemCard key={index} data={item} />))}
           </OwlCarousel>}
         </div>
       </div>
